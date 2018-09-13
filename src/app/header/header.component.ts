42e2281b4d889327f1a9cbbe9029dbfa54ab69c9
Will <doggy.huang@gmail.com>
2018-09-13 13:53:41 +0800

    屬性繫結 (Property Binding)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   sitename = 'Hello World';
+  sitelink = 'http://blog.miniasp.com/';
+  subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
 
   constructor() { }
 
